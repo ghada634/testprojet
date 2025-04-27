e79748db10f131744f5c57a93e15d6e5bd7c9fe2
ghada634 <ghadaderouiche8@gmail.com>
2025-04-27 20:48:06 +0100

    aws

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 
 function OpenCon()
 {
-    $conn = new mysqli("db", "root", "", "edoc");
+    $conn = new mysqli("172.31.88.204", "root", "", "edoc");
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
